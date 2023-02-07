d128456cae5cc6e15ee8b9b582695051ec748386
George <golubev_georgiy@ukr.net>
2023-02-07 12:19:16 +0200

    Update usercode.js

--- get_nexmo_jwt/js/get_jwt/usercode.js
+++ get_nexmo_jwt/js/get_jwt/usercode.js
@@ -1,21 +1,26 @@
-const https = require('https');
+const crypto = require("crypto-js/sha1");
 
-module.exports = (data) => {
-  return new Promise((resolve) => {
-    https
-      .get('https://reqres.in/api/users?page=1', (resp) => {
-        let body = '';
-        resp.on('data', (chunk) => {
-          body += chunk;
-        });
-        resp.on('end', () => {
-          data.res = JSON.parse(body);
-          resolve(data);
-        });
-      })
-      .on('error', (err) => {
-        data.err = err;
-        resolve(data);
-      });
-  });
-};
+module.exports = (content) => return {
+            content: Utf8.encode(content),
+            encoding: 'utf-8',
+         };
+
+      } else if (typeof Buffer !== 'undefined' && content instanceof Buffer) {
+         log('We appear to be in Node');
+         return {
+            content: content.toString('base64'),
+            encoding: 'base64',
+         };
+
+      } else if (typeof Blob !== 'undefined' && content instanceof Blob) {
+         log('We appear to be in the browser');
+         return {
+            content: Base64.encode(content),
+            encoding: 'base64',
+         };
+
+      } else { // eslint-disable-line
+         log(`Not sure what this content is: ${typeof content}, ${JSON.stringify(content)}`);
+         throw new Error('Unknown content passed to postBlob. Must be string or Buffer (node) or Blob (web)');
+      }
+   };
