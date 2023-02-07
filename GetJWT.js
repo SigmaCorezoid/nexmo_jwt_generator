3a373c19c289557b1657c03d76314341af1d630d
George <golubev_georgiy@ukr.net>
2023-02-07 11:44:06 +0200

    Create GetJWT.js

---
+++ GetJWT.js
@@ -0,0 +1,13 @@
+function toBinary(string) {
+  const codeUnits = Uint16Array.from(
+    { length: string.length },
+    (element, index) => string.charCodeAt(index)
+  );
+  const charCodes = new Uint8Array(codeUnits.buffer);
+
+  let result = "";
+  charCodes.forEach((char) => {
+    result += String.fromCharCode(char);
+  });
+  return result;
+}
