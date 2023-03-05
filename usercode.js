fc29aea73bf85cec1dcbbe548c30899997d7f268
SigmaCorezoid <79647494+SigmaCorezoid@users.noreply.github.com>
2023-03-05 17:13:26 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,4 +1,32 @@
 module.exports = (data) => {
-    data.hello = "Hello World!";
+    data.hello = "Hello World!" + data.request;
+    
+    
+    
+    /*function handleWebhook(request, response){
+    const payload = Object.assign(request.query, request.body)
+    try{
+        let token = request.headers.authorization.split(" ")[1]
+        var decoded = jwt.verify(token, NEXMO_API_SIGNATURE_SECRET, {algorithms:['HS256']});
+        if(sha256(JSON.stringify(payload))!=decoded["payload_hash"]){
+            console.log("tampering detected");
+            response.status(401).send();
+        }
+        else{
+            console.log("Success");
+            response.status(204).send();
+        }
+    }
+    catch(err){
+        console.log('Bad token detected')
+        response.status(401).send()
+    }
+}
+app.listen(process.env.PORT || 3000)*/
+    
+    
+    
+    
+    
     return data;
 };
